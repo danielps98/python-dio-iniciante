691664ccef192a9e742128917e3cf8c10bd32fa5
danielpereiradasilva <danielpdsnpk@gmail.com>
2025-04-01 17:30:12 -0300

    comite

--- print-input.py
+++ print-input.py
@@ -3,5 +3,5 @@ nome =  input("informe seu nome :")
 idade = input("informe sua idade :")
 
 print ( nome ,"sua e "  )
-print("texte" ,end="....\n")
-print (idade, idade , end="....\n")
+print("texte" ,end="...\n")
+print (idade, idade , end="...\n")
